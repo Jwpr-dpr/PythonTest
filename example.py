036ad5011693847bfd312a130771a213aeb950a9
jwpr-dpr <jwprdpr@gmail.com>
2023-03-02 15:02:49 -0500

    first commit

---
+++ example.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a+b
+
+def test_add():
+    assert add(1,2) == 3
+    assert add('space','ship') == 'spaceship'
+
+def substract(a,b):
+    return a+b
+
+#def test_substract():
+#    assert substract(1-1) == 0
+
